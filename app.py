7b7e4ab280370358d4bff2c949f9bc4e68a39b4b
Austin Clark <asclark109@uchicago.edu>
2022-02-13 13:15:38 -0600

    add initial work

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from calendar import c
 import string
 import random
-from datetime import datetime
-from flask import Flask, render_template, request
+from datetime import datetime, timedelta
+from flask import Flask, render_template, request, jsonify
 from functools import wraps
+from itsdangerous import json
+
+from sqlalchemy import true
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -48,9 +52,17 @@ def index(chat_id=None):
 def auth():
     return app.send_static_file('username.html')
 
+@app.route('/authenticate')
+def auth():
+    return app.send_static_file('auth.html')
+
 @app.route('/chat/<int:chat_id>')
 def chat(chat_id):
-    invite_link = "some_cool_invite_link"
+    # we know the chat id, and we need to look up the magic key
+    # to render the site properly
+    global chats
+    magic_key = chats[chat_id]["magic_key"]
+    invite_link = "?chat_id={}&magic_key={}".format(chat_id,magic_key)
     return render_template('chat.html',
             chat_id=chat_id,
             invite_link=invite_link)
@@ -58,3 +70,299 @@ def chat(chat_id):
 # -------------------------------- API ROUTES ----------------------------------
 
 # TODO: Create the API
+
+new_chat_id_number = 1
+new_session_token_number = 1
+
+def generate_session_token():
+    """returns a new unique session token"""
+    global new_session_token_number
+    assignment = new_session_token_number
+    new_session_token_number += 1
+    return "session_token_{}".format(new_session_token_number)
+
+def generate_new_chat_id():
+    """returns a new unique chat id"""
+    global new_chat_id_number
+    assigned_chat_id = new_chat_id_number
+    new_chat_id_number +=1
+    return assigned_chat_id
+
+@app.route('/create', methods = ['POST'])
+def create():
+    # create new chat with new secret invite link
+    global chats
+
+    # receive request
+    content = request.get_json()
+
+    # expecting username to be defined
+    print(content["username"])
+    host_username = content["username"]
+
+    # generate session token
+    chat_id = generate_new_chat_id()
+    session_token = generate_session_token()
+    print(chat_id)
+    print(session_token)
+
+    # create new chat
+    new_chat_dict = newChat(host_username,session_token)
+    print(new_chat_dict)
+
+    # add chat to chats dict
+    chats[chat_id] = new_chat_dict
+    print()
+    print(chats)
+ 
+    # return unique chat_id, and session_token to allow creator to identify themselves
+    # in subsequent requests
+    return jsonify({"session_token" : session_token,
+                    "chat_id" : chat_id})
+
+
+@app.route('/chat/<chat_id>', methods = ['GET','POST'])
+def func(chat_id):
+    global chats
+    ### POST REQUEST
+    if request.method == 'GET':
+        # require a valid session_token in an authorization header
+        # return the messages in the chat.
+        # take optional param that only returns new messages
+
+        print("ARRIVED")
+
+        # access global chats dict datastructure
+        
+
+        # receive request
+        content = request.get_json()
+        print(content)
+
+        # validate user token
+        user_token = request.headers.get("user_token")
+
+        if user_token in chats[int(chat_id)]["authorized_users"].keys():
+            print("token validated!: "+user_token)
+
+            print("now: "+ str(datetime.utcnow()))
+            expiration = chats[int(chat_id)]["authorized_users"][user_token]["expires"]
+
+            # check if token expired
+            if datetime.utcnow() > expiration:
+                print("EXPIRED TOKEN")
+                print("TO IMPLMEMENT: REMOVE TOKEN")
+            else:
+                print("expiration: "+str(expiration))
+                print("token not expired!")
+
+                # package up and send all the messages
+                return jsonify({"messages" : chats[int(chat_id)]["messages"]})
+        # return empty dict of messages
+        return jsonify({"messages" : dict()})
+    ### GET REQUEST
+    if request.method == 'POST':
+        print("ARRIVED")
+
+        # receive request
+        content = request.get_json()
+        print(content)
+
+        # validate user token
+        user_token = request.headers.get("user_token")
+
+        if user_token in chats[int(chat_id)]["authorized_users"].keys():
+            print("token validated!: "+user_token)
+
+            print("now: "+ str(datetime.utcnow()))
+            expiration = chats[int(chat_id)]["authorized_users"][user_token]["expires"]
+
+            # check if token expired
+            if datetime.utcnow() > expiration:
+                print("EXPIRED TOKEN")
+                print("TO IMPLMEMENT: REMOVE TOKEN")
+            else:
+                print("expiration: "+str(expiration))
+                print("token not expired!")
+
+                # display comment
+                user_comment = content['comment']
+                print("user comment: "+user_comment)
+
+                # add comment to data structure on server
+                # tracking chat data
+                list_of_comment_dicts = chats[int(chat_id)]["messages"]
+                new_comment_dict = dict()
+                # lookup username
+                username = chats[int(chat_id)]["authorized_users"][user_token]["username"]
+                body = user_comment
+                new_comment_dict = {"username": username, "body": body}
+                print(new_comment_dict)
+                list_of_comment_dicts.append(new_comment_dict)
+                # update chat comments
+                chats[int(chat_id)]["messages"] = list_of_comment_dicts
+                # see updated chat data
+                print(chats)    
+
+                # # require a valid session_token in an authorization header
+                # post a new message to the chat
+                return jsonify({"status" : "successful posting of comment",
+                })
+        # # require a valid session_token in an authorization header
+        # post a new message to the chat
+        return jsonify({"status" : "failed to post comment",
+        })
+
+# @app.route('/chat/<chat_id>', methods = ['POST'])
+# def func2(chat_id):
+#     print("ARRIVED")
+#     # access global chats dict datastructure
+#     global chats
+
+#     # receive request
+#     content = request.get_json()
+#     print(content)
+
+#     # validate user token
+#     user_token = request.headers.get("user_token")
+
+#     if user_token in chats[int(chat_id)]["authorized_users"].keys():
+#         print("token validated!: "+user_token)
+
+#         print("now: "+ str(datetime.utcnow()))
+#         expiration = chats[int(chat_id)]["authorized_users"][user_token]["expires"]
+
+#         # check if token expired
+#         if datetime.utcnow() > expiration:
+#             print("EXPIRED TOKEN")
+#             print("TO IMPLMEMENT: REMOVE TOKEN")
+#         else:
+#             print("expiration: "+str(expiration))
+#             print("token not expired!")
+
+#             # display comment
+#             user_comment = content['comment']
+#             print("user comment: "+user_comment)
+
+#             # add comment to data structure on server
+#             # tracking chat data
+#             list_of_comment_dicts = chats[int(chat_id)]["messages"]
+#             new_comment_dict = dict()
+#             # lookup username
+#             username = chats[int(chat_id)]["authorized_users"][user_token]["username"]
+#             body = user_comment
+#             new_comment_dict = {"username": username, "body": body}
+#             print(new_comment_dict)
+#             list_of_comment_dicts.append(new_comment_dict)
+#             # update chat comments
+#             chats[int(chat_id)]["messages"] = list_of_comment_dicts
+#             # see updated chat data
+#             print(chats)    
+
+#             # # require a valid session_token in an authorization header
+#             # post a new message to the chat
+#             return jsonify({"status" : "successful posting of comment",
+#             })
+#     # # require a valid session_token in an authorization header
+#     # post a new message to the chat
+#     return jsonify({"status" : "failed to post comment",
+#     })
+
+
+@app.route('/api/get_messages', methods = ['GET'])
+def func3():
+    global chats
+    ### POST REQUEST
+    if request.method == 'GET':
+        # require a valid session_token in an authorization header
+        # return the messages in the chat.
+        # take optional param that only returns new messages
+
+        print("ARRIVED: MESSAGES")
+
+        # access global chats dict datastructure
+        
+
+        # receive request
+        content = request.get_json()
+        print(content)
+
+        # validate user token
+        user_token = request.headers.get("user_token")
+        chat_id = request.headers.get("chat_id")
+        print(user_token)
+        print(chat_id)
+
+        if user_token in chats[int(chat_id)]["authorized_users"].keys():
+            print("token validated!: "+user_token)
+
+            print("now: "+ str(datetime.utcnow()))
+            expiration = chats[int(chat_id)]["authorized_users"][user_token]["expires"]
+
+            # check if token expired
+            if datetime.utcnow() > expiration:
+                print("EXPIRED TOKEN")
+                print("TO IMPLMEMENT: REMOVE TOKEN")
+            else:
+                print("expiration: "+str(expiration))
+                print("token not expired!")
+
+                # package up and send all the messages
+                return jsonify({"messages" : chats[int(chat_id)]["messages"]})
+        # return empty dict of messages
+        return jsonify({"messages" : dict()})
+    ### GET REQUEST
+    if request.method == 'POST':
+        print("ARRIVED")
+
+        # receive request
+        content = request.get_json()
+        print(content)
+
+        # validate user token
+        user_token = request.headers.get("user_token")
+
+        if user_token in chats[int(chat_id)]["authorized_users"].keys():
+            print("token validated!: "+user_token)
+
+            print("now: "+ str(datetime.utcnow()))
+            expiration = chats[int(chat_id)]["authorized_users"][user_token]["expires"]
+
+            # check if token expired
+            if datetime.utcnow() > expiration:
+                print("EXPIRED TOKEN")
+                print("TO IMPLMEMENT: REMOVE TOKEN")
+            else:
+                print("expiration: "+str(expiration))
+                print("token not expired!")
+
+                # display comment
+                user_comment = content['comment']
+                print("user comment: "+user_comment)
+
+                # add comment to data structure on server
+                # tracking chat data
+                list_of_comment_dicts = chats[int(chat_id)]["messages"]
+                new_comment_dict = dict()
+                # lookup username
+                username = chats[int(chat_id)]["authorized_users"][user_token]["username"]
+                body = user_comment
+                new_comment_dict = {"username": username, "body": body}
+                print(new_comment_dict)
+                list_of_comment_dicts.append(new_comment_dict)
+                # update chat comments
+                chats[int(chat_id)]["messages"] = list_of_comment_dicts
+                # see updated chat data
+                print(chats)    
+
+                # # require a valid session_token in an authorization header
+                # post a new message to the chat
+                return jsonify({"status" : "successful posting of comment",
+                })
+        # # require a valid session_token in an authorization header
+        # post a new message to the chat
+        return jsonify({"status" : "failed to post comment",
+        })
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -3,7 +3,50 @@
 // TODO: If a user clicks to create a chat, create a session token for them
 // and save it. Redirect the user to /chat/<chat_id>
 function createChat() {
+  
+    if (sessionStorage.username === undefined) {
+      location.href = "/username";
+    }
+    else {
+      console.log("lets create a chat!")
+      // we have a username defined. create a chat. prepare
+      // to send the create request to the python API
+      host_username = sessionStorage.username
 
+      options = {
+        method: 'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          'username': host_username,
+        })
+      };
+
+      console.log("sending request to python API / server")
+      // send request and get response...ultimately stored in "json"
+      fetch( '/create', options).then((response) => {
+        console.log(response);
+        return response.json()
+      }).then((json) => {
+        console.log(json);
+        console.log("obtained response from server! parsed to JSON!")
+
+      // response should contain host_session_token and chat_id
+      // print chat id
+      console.log("Storing chat_id: ",json.chat_id);
+      // sessionStorage.setItem("session_token",json.session_token)
+
+      // Store Session Token
+      console.log("Storing Session_token: ",json.session_token);
+      sessionStorage.setItem("session_token",json.session_token)
+  
+      // Redirect to chat
+      // console.log(redirect)
+      window.location.href = "/chat/" + String(json.chat_id);;
+      
+      });
+    }
 }
 
 /* For auth.html */
@@ -13,6 +56,9 @@ function createChat() {
 // If the user authenticaes successfully, save the session token
 // and redirect them to /chat/<chat_id>
 function authenticate() {
+  // okay, we want to pull out of the url the chat_id and magic_key,
+  // if there is a failure, we redirect to the homepage.
+  
   return;
 }
 
@@ -22,13 +68,158 @@ function authenticate() {
 // POST to the API when the user posts a new message.
 // Automatically poll for new messages on a regular interval.
 function postMessage() {
-  return;
+
+  // if (localStorage.username === undefined) {
+  //   location.href = "/username";
+  // }
+  // else {
+    console.log("lets post a message!")
+    // prepare to send the post message request to the python API
+  
+    // get token of user (user needs a token if they are to be on this page)
+    user_token = sessionStorage.session_token
+  
+    comment = document.getElementById("messageBox").value
+  
+    options = {
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json',
+        'user_token': user_token
+      },
+      body: JSON.stringify({
+        'comment': comment
+      })
+    };
+  
+    console.log("sending request to python API / server: post message")
+    // send request and get response...ultimately stored in "json"
+    // get current chat_id
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams);
+    // if (urlParams.has('chat_id')
+  
+    fetch( '/chat/1', options).then((response) => {
+      console.log(response);
+      return response.json()
+    }).then((json) => {
+      console.log(json);
+      console.log("obtained response from server! parsed to JSON!");
+  
+    // response should contain host_session_token and chat_id
+    // print chat id
+    console.log("comment submission status: ",json.status);
+    // sessionStorage.setItem("session_token",json.session_token)
+  
+    // Redirect to chat
+    // console.log(redirect)
+    // window.location.href = "/chat/" + String(json.chat_id);;
+    
+    });
+    //}
+    return;
 }
 
 function getMessages() {
+  // if (localStorage.username === undefined) {
+  //   location.href = "/username";
+  // }
+  // else {
+  console.log("let's obtain the messages!")
+  // prepare to send the post message request to the python API
+
+  // get token of user (user needs a token if they are to be on this page)
+  user_token = sessionStorage.session_token
+
+  // options = {
+  //   method: 'GET',
+  //   headers:{
+  //     'Accept': 'application/json',
+  //     'user_token': user_token
+  //   }
+  // };
+
+  options = {
+    method: 'GET',
+    headers:{
+      'Accept': 'application/json',
+      'user_token': user_token,
+      'chat_id': "1",
+      'new': "true"
+    }
+  };
+
+  console.log(options);
+
+  console.log("sending request to python API / server: get messages")
+  // send request and get response...ultimately stored in "json"
+  // get current chat_id
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams);
+  // if (urlParams.has('chat_id')
+
+  fetch( '/api/get_messages', options).then((response) => {
+    console.log(response);
+    return response.json()
+  }).then((json) => {
+    console.log(json);
+    console.log("obtained response from server! parsed to JSON!");
+  // fetch( '/chat/1', options).then((response) => {
+  //   console.log(response);
+  //   return response.json()
+  // }).then((json) => {
+  //   console.log(json);
+  //   console.log("obtained response from server! parsed to JSON!");
+
+  msgs= json.messages
+  // reponse should have a dict of messages:
+
+  // re-write messages
+  // first erase them
+  commentContainer = document.querySelector(".messages")
+  while (commentContainer.innerHTML.trim() != ""){
+    commentContainer.lastElementChild.remove();
+  }
+
+  // write the messages
+  for (let i = 0; i < msgs.length; i++) {
+    let body = msgs[i]["body"];
+    let username = msgs[i]["username"];
+    message_elem = document.createElement('message');
+    author_elem = document.createElement('author');
+    author_elem.textContent = username;
+    content_elem = document.createElement('content');
+    content_elem.textContent = body;
+    message_elem.appendChild(author_elem)
+    message_elem.appendChild(content_elem)
+    commentContainer.appendChild(message_elem)
+  }
+
+
+
+
+  
+
+  
+  // response should contain host_session_token and chat_id
+  // print chat id
+  // console.log("comment submission status: ",json.status);
+  // sessionStorage.setItem("session_token",json.session_token)
+
+  // Redirect to chat
+  // console.log(redirect)
+  //window.location.href = "/chat/" + String(json.chat_id);;
+  
+  });
+  //}
+  
   return;
 }
 
 function startMessagePolling() {
+  // var intervalID = setInterval(getMessages,500);
+  // setTimeout(function() {
+  //   console.log("looking for content");
+  // }, 4 * 1000);
   return;
 }
